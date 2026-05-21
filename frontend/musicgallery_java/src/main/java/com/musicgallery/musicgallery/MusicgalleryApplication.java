1cf4e7f5c292e361262783a5c56cc9b20309399d
esevaliloko <raulvalientes5@gmail.com>
2026-05-21 10:28:14 +0200

    Estructura incial

---
+++ frontend/musicgallery_java/src/main/java/com/musicgallery/musicgallery/MusicgalleryApplication.java
@@ -0,0 +1,13 @@
+package com.musicgallery.musicgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicgalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicgalleryApplication.class, args);
+	}
+
+}
