afaf48c24a6c1e5d8929bafc1ede00eb7d3000a1
KhurinGoro <jmajanj@gmail.com>
2026-05-21 12:02:40 +0200

    fix: corregido TiendaRepository

--- backend/src/main/java/com/musicgallery/musicgallery/repository/TiendaRepository.java
+++ backend/src/main/java/com/musicgallery/musicgallery/repository/TiendaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TiendaRepository extends JpaRepositoryTienda, Long {
+public interface TiendaRepository extends JpaRepository<Tienda, Long> {
 }
